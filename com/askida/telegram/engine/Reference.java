804ca855a5add5814adbbd4bad310fa341249c0f
AsKidaHikka <40733119+AsKidaHikka@users.noreply.github.com>
2018-07-01 02:34:19 -0700

    Update Reference.java

--- com/askida/telegram/engine/Reference.java
+++ com/askida/telegram/engine/Reference.java
@@ -3,8 +3,8 @@ package com.askida.telegram.engine;
 public class Reference {
 
     public static final String HOST = "https://api.telegram.org/bot";
-    public static final String TOKEN = "606122268:AAFro67SmZNSl115rkaidsFLAr48zH6CeoE";
+    public static final String TOKEN = "Use your bot token here";
 
-    public static int ChatID = -163021482;
+    public static int ChatID = (use your chat ID);
 
 }
